82eb99ac191bbc28032b7bbdfefb18ab576af764
Arber Kuci <arber.kuci2@gmail.com>
2018-12-16 20:17:19 +0100

    T57: Configured services in docker-compose file for influxdb, mongodb and server.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-echo 'Cleaning old docker images and containers.'
-docker stop thingy-client-purple
-docker rm thingy-client-purple
-docker rmi harperkej/thingy-client-purple
 
-cd ../
+cd ..
 cp -r ./thing-client-purple ./thingy-deployment
 cp -r ./thingy-api-purple ./thingy-deployment
 
 cd ./thingy-deployment/thing-client-purple
 echo 'Building the client!'
 ./build-client.sh
-cd ..
+cd ../thingy-api-purple
+echo 'Building the server!'
+./build-server.sh
 rm -r ./thingy-api-purple
 rm -r ./thing-client-purple
 
